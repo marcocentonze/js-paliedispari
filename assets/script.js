a9a2716fee26e07206aa68fc505868c61df2b124
marcocentonze <marco.centonze27@gmail.com>
2023-08-31 16:08:22 +0200

    add first logic for the solution

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,41 @@
 /* Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
-   Palidroma
-Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+  
    Consigli del giorno
-Scriviamo sempre in italiano i passaggi che vogliamo fare
-Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+-Scriviamo sempre in italiano i passaggi che vogliamo fare
+-Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 non partiamo a bomba con la scrittura delle funzioni, ma prima scriviamo la logica come facciamo di solito, poi passiamo al refactoring e cerchiamo di capire cosa sia meglio creare come funzione.*/
+
+  //prompt pari o dispari
+const pariODispari = prompt('pari o dispari?');
+console.log(pariODispari);
+
+  //prompt numero da 1 a 5
+const numeroUtente = Number(prompt('Scegli un numero da 1 a 5'));
+console.log(numeroUtente);
+
+  //generiamo numero random(da 1 a 5)con una funzione
+  let numeroRandomPc = Math.floor((Math.random() * 5) + 1);
+  console.log(numeroRandomPc);
+  //sommiamo i due numeri 
+  let sum = 0;
+sum  = numeroUtente + numeroRandomPc;
+console.log(sum);
+//creiamo una variabile per dire se è pari o dispari
+  let numeroPari= true;
+  let numeroDispari =false;
+  //stabiliamo se la somma dei due numeri è pari o dispari
+if (sum % 2 === 0) {
+    sum = numeroPari ;
+    console.log(sum);
+} else {
+   (sum % 2 !== 0)
+   sum = numeroDispari;
+   console.log(sum);
+} 
+//dichiariamo il vincitore
+
+
+  
+
